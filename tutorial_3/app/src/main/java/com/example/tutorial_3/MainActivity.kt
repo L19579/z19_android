77e3269b9d57baed0d8358ff67d4693160fb73a9
JoJo <l19priv@gmail.com>
2023-11-12 13:28:50 -0700

    tutorial_3 complete

--- tutorial_3/app/src/main/java/com/example/tutorial_3/MainActivity.kt
+++ tutorial_3/app/src/main/java/com/example/tutorial_3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tutorial_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -48,41 +50,55 @@ private fun QuadrantApp(){
         Modifier
             /*.fillMaxHeight()*/
             .fillMaxSize()
-            .border(1.dp, Color.Red)
+            /*.border(1.dp, Color.Red)*/
     ){
         Row(
             Modifier
                 .weight(1f)
                 .fillMaxWidth()
-                .border(10.dp, Color.Blue)
+                /*.border(10.dp, Color.Blue)*/
         ){
-            Quadrant(text_title = stringResource(R.string.reference_name), text_body = "bye 1",
+            Quadrant(
+                text_title = stringResource(R.string.quadrant_one_title),
+                text_body = stringResource(R.string.quadrant_one_description),
+                panel_color = Color(0xFFEADDFF),
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(0.5f)
+                    .fillMaxSize()
+                    /*.fillMaxWidth(0.5f)*/
             )
-            Quadrant(text_title = "hi 2", text_body = "bye 2",
+            Quadrant(
+                text_title = stringResource(R.string.quadrant_two_title),
+                text_body = stringResource(R.string.quadrant_two_description),
+                panel_color = Color(0xFFD0BCFF),
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(1f)
+                    .fillMaxSize()
+                    /*.fillMaxWidth(1f)*/
                 )
         }
         Row(
             Modifier
                 .weight(1f)
                 .fillMaxWidth()
-                .border(10.dp, Color.Blue),
+                /*.border(10.dp, Color.Blue),*/
             /*horizontalArrangement = Arrangement.SpaceAround*/
         ){
-            Quadrant(text_title = "hi 3", text_body = "bye 3",
+            Quadrant(
+                text_title = stringResource(R.string.quadrant_three_title),
+                text_body = stringResource(R.string.quadrant_three_description),
+                panel_color = Color(0xFFB69DF8),
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(0.5f)
+                    .fillMaxSize()
             )
-            Quadrant(text_title = "hi 4", text_body = "bye 4",
+            Quadrant(
+                text_title = stringResource(R.string.quadrant_four_title),
+                text_body = stringResource(R.string.quadrant_four_description),
+                panel_color = Color(0xFFF6EDFF),
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(1f)
+                    .fillMaxSize()
             )
         }
     }
@@ -92,30 +108,36 @@ private fun QuadrantApp(){
 private fun Quadrant(
     text_title: String,
     text_body: String,
-    panel_color: Color = Color.Red,
+    panel_color: Color = Color.LightGray,
     modifier: Modifier = Modifier){
     Column(
-        modifier = Modifier
+        modifier = modifier
             /*:.fillMaxSize()*/
-            .border(1.dp, Color.Red)
+            .border(1.dp, Color.Black)
+            .background(panel_color)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
         Text(
             text = text_title,
-            fontSize = 20.sp,
+            fontSize = 16.sp,
             fontWeight = FontWeight.Bold,
-            color = Color.White,
+            color = Color.Black,
             textAlign = TextAlign.Center,
-            modifier = modifier
-                .padding(5.dp)
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 0.dp, 15.dp)
+                /*.border(1.dp, Color.Green)*/
         )
 
         Text(
             text = text_body,
             fontSize = 10.sp,
-            color = Color.White,
+            color = Color.Black,
             textAlign = TextAlign.Center,
-            modifier = modifier
+            modifier = Modifier
                 .padding(16.dp)
+                /*.border(1.dp, Color.Green)*/
         )
     }
 }

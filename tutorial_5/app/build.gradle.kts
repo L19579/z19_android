a440840834e30945d67e7d29b8d18802d69ab645
JoJo <l19priv@gmail.com>
2023-11-13 19:16:02 -0700

    tutorial_5 complete

--- tutorial_5/app/build.gradle.kts
+++ tutorial_5/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tutorial_5"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tutorial_5"
--- tutorial_5/app/src/main/java/com/example/tutorial_5/MainActivity.kt
+++ tutorial_5/app/src/main/java/com/example/tutorial_5/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.tutorial_5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tutorial_5.ui.theme.Tutorial_5Theme
 import androidx.compose.material3.Button
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    DiceRollerApp()
                 }
             }
         }
@@ -53,11 +64,34 @@ fun DiceWithButtonAndImage(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Button( onClick = { /* TODO */ }){
-            Text(stringResource(R.string.roll))
+        /*var result = 1*/
+        var result by remember { mutableStateOf(1) }
+        val painter = when(result){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        Image(
+            painter = painterResource(painter),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button( onClick = {
+            result = (1..6).random()
+            Log.d("button detection log",
+                "hello from logger. result value: {$result}")
+        }){
+            /*Text(stringResource(R.string.roll))*/
+            Text(result.toString())
         }
     }
 }
+
+
 
 @Composable
 fun GreetingPreview() {
